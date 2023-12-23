b4578b76ed5362dfee8e282f8e9a9bb478ed6b21
Marian <marianmykola88@gmail.com>
2023-12-23 08:41:45 +0100

    Initial

---
+++ project/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<!-- HelloWorld.vue -->
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <input v-model="userInput" placeholder="Type a message...">
+    <p>You typed: {{ userInput }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello, Vue!',
+      userInput: ''
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* Add optional scoped styles here */
+</style>
